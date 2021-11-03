c482ca0d8b2b0ee8b89c839ef7f88dfe43155d40
Marcelo Barboza <marcelo.barboza.cmkp@gmail.com>
2021-11-03 15:28:18 -0300

    feito model de postagem v1

---
+++ blogpessoal/src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -0,0 +1,68 @@
+package com.generation.blogpessoal.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
+@Table(name = "tb_postagem")
+public class Postagem {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) 
+	private long id;
+	
+	@NotBlank(message = "O atributo título é Obrigatório!") 
+	@Size(min = 5, max = 100, message = "O atributo título deve conter no mínimo 05 e no máximo 100 caracteres")
+	private String titulo;
+	
+	@NotBlank(message = "O atributo texto é Obrigatório!")
+	@Size(min = 10, max = 1000, message = "O atributo texto deve conter no mínimo 10 e no máximo 500 caracteres")
+	private String texto;
+	
+	@UpdateTimestamp
+	private LocalDate data;
+	
+	/*Insira os Getters and Setters*/
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public LocalDate getData() {
+		return data;
+	}
+
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+
+}
