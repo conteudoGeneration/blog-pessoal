d14b71d3492369917580e075742ebd7ae642538f
Marcelo Barboza <60588536+Marcelo7211@users.noreply.github.com>
2021-11-08 10:42:53 -0300

    Update UsuarioService.java

--- blogpessoal/src/main/java/com/generation/blogpessoal/service/UsuarioService.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/service/UsuarioService.java
@@ -61,6 +61,7 @@ public class UsuarioService {
 
 				usuarioLogin.get().setId(usuario.get().getId());				
 				usuarioLogin.get().setNome(usuario.get().getNome());
+				usuarioLogin.get().setFoto(usuario.get().getFoto());
 				usuarioLogin.get().setSenha(usuario.get().getSenha());
 				usuarioLogin.get().setToken(authHeader);
 
