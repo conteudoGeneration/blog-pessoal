7f2566200c27b6bef82282f73fe7a76661a8ed85
Marcelo Barboza <60588536+Marcelo7211@users.noreply.github.com>
2021-11-10 10:09:52 -0300

    Update UserDetailsServiceImpl.java

--- blogpessoal/src/main/java/com/generation/blogpessoal/security/UserDetailsServiceImpl.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/security/UserDetailsServiceImpl.java
@@ -3,12 +3,10 @@ package com.generation.blogpessoal.security;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.blogpessoal.model.Usuario;
 import com.generation.blogpessoal.repository.UsuarioRepository;
@@ -29,11 +27,8 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
 
 		Optional<Usuario> usuario = repository.findByUsuario(userName);
-		
-		if (usuario.isPresent()) {
-			return new UserDetailsImpl(usuario.get());
-		} else {
-			throw new ResponseStatusException(HttpStatus.FORBIDDEN);
-		}
+		usuario.orElseThrow(() -> new UsernameNotFoundException(userName + " not found."));
+
+		return usuario.map(UserDetailsImpl::new).get();
 	}
 }
